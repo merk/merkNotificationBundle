2fb73b1db63098e6e408e17d2ee56a428a6f800b
IamPersistent <develop@zestic.com>
2011-08-30 14:54:36 -0700

    DRY up the features

--- Features/Context/FeatureContext.php
+++ Features/Context/FeatureContext.php
@@ -25,21 +25,23 @@ use merk\NotificationBundle\Features\Entity\Post;
 class FeatureContext extends BehatContext //MinkContext if you want to test web
 {
     /**
-     * @Given /^SiteOwner creates a new post named "([^"]*)"$/
+     * @Given /^"([^"]*)" creates a new post named "([^"]*)"$/
      */
-    public function siteOwnerCreatesANewPostNamed($name)
+    public function createsANewPostNamed($author, $name)
     {
         $om = $this->getContainer()->get('doctrine.orm.default_entity_manager');
         $post = new Post();
         $post->setName($name);
         $om->persist($post);
         $om->flush();
+
+        throw new \Exception('add author support');
     }
 
     /**
-     * @Given /^the SiteOwner publishes "([^"]*)"$/
+     * @Given /^the "([^"]*)" publishes the post "([^"]*)"$/
      */
-    public function theSiteOwnerPublishes($argument1)
+    public function thePublishesThePost($author, $postName)
     {
         throw new PendingException();
     }
@@ -53,89 +55,41 @@ class FeatureContext extends BehatContext //MinkContext if you want to test web
     }
 
     /**
-     * @Given /^a reference to the new post is persisted as the object of the notification$/
+     * @Given /^a reference to "([^"]*)" is persisted as the "([^"]*)" of the notification$/
      */
-    public function aReferenceToTheNewPostIsPersistedAsTheObjectOfTheNotification()
+    public function aReferenceToIsPersistedAsTheOfTheNotification($object, $part)
     {
         throw new PendingException();
     }
 
     /**
-     * @Given /^publish is persisted as the verb of the notification$/
+     * @Given /^"([^"]*)" is persisted as the verb of the notification$/
      */
-    public function publishIsPersistedAsTheVerbOfTheNotification()
+    public function isPersistedAsTheVerbOfTheNotification($argument1)
     {
         throw new PendingException();
     }
 
     /**
-     * @Given /^a reference to SiteOwner is persisted as the actor of the notification$/
+     * @Given /^the "([^"]*)" updates the post "([^"]*)"$/
      */
-    public function aReferenceToSiteOwnerIsPersistedAsTheActorOfTheNotification()
+    public function theUpdatesThePost($author, $argument2)
     {
         throw new PendingException();
     }
 
     /**
-     * @Given /^the SiteOwner updates "([^"]*)"$/
+     * @Given /^"([^"]*)" creates a comment on the post "([^"]*)"$/
      */
-    public function theSiteOwnerUpdates($argument1)
+    public function createsACommentOnThePost($author, $postName)
     {
         throw new PendingException();
     }
 
     /**
-     * @Given /^update is persisted as the verb of the notification$/
+     * @Given /^"([^"]*)" deletes the post "([^"]*)"$/
      */
-    public function updateIsPersistedAsTheVerbOfTheNotification()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Given /^SiteGuest creates a comment on "([^"]*)"$/
-     */
-    public function siteGuestCreatesACommentOn($argument1)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Given /^a reference to the new comment is persisted as the object of the notification$/
-     */
-    public function aReferenceToTheNewCommentIsPersistedAsTheObjectOfTheNotification()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Given /^add is persisted as the verb of the notification$/
-     */
-    public function addIsPersistedAsTheVerbOfTheNotification()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Given /^a reference to SiteGuest is persisted as the actor of the notification$/
-     */
-    public function aReferenceToSiteGuestIsPersistedAsTheActorOfTheNotification()
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Given /^SiteOwner deletes "([^"]*)"$/
-     */
-    public function siteOwnerDeletes($argument1)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Given /^delete is persisted as the verb of the notification$/
-     */
-    public function deleteIsPersistedAsTheVerbOfTheNotification()
+    public function deletesThePost($author, $postName)
     {
         throw new PendingException();
     }
--- Features/notify.feature
+++ Features/notify.feature
@@ -3,34 +3,33 @@ Feature: notify
     As a developer
     I need to use annotations to give instructions on notifying blog activities
 
-Scenario: Trigger notification that SiteOwner created a post
-    Given SiteOwner creates a new post named "My New Post"
-    And the SiteOwner publishes "My New Post"
+Scenario: Trigger notification that "SiteOwner" created a post
+    Given "SiteOwner" creates a new post named "My New Post"
+    And the "SiteOwner" publishes the post "My New Post"
     Then a notification is created
-    And a reference to the new post is persisted as the object of the notification
-    And publish is persisted as the verb of the notification
-    And publish is persisted as the verb of the notification
-    And a reference to SiteOwner is persisted as the actor of the notification
+    And a reference to "My New Post" is persisted as the "object" of the notification
+    And "publish" is persisted as the verb of the notification
+    And a reference to "SiteOwner" is persisted as the "actor" of the notification
 
-Scenario: Trigger notification that SiteOwner updated a post
-    Given SiteOwner creates a new post named "My New Post"
-    And the SiteOwner publishes "My New Post"
-    And the SiteOwner updates "My New Post"
+Scenario: Trigger notification that "SiteOwner" updated a post
+    Given "SiteOwner" creates a new post named "My New Post"
+    And the "SiteOwner" publishes the post "My New Post"
+    And the "SiteOwner" updates the post "My New Post"
     Then a notification is created
-    And a reference to the new post is persisted as the object of the notification
-    And update is persisted as the verb of the notification
-    And a reference to SiteOwner is persisted as the actor of the notification
+    And a reference to "My New Post" is persisted as the "object" of the notification
+    And "update" is persisted as the verb of the notification
+    And a reference to "SiteOwner" is persisted as the "actor" of the notification
 
-Scenario: Trigger notification that SiteGuest commented on a post
-    Given SiteGuest creates a comment on "My New Post"
+Scenario: Trigger notification that "SiteGuest" commented on a post
+    Given "SiteGuest" creates a comment on the post "My New Post"
     Then a notification is created
-    And a reference to the new comment is persisted as the object of the notification
-    And add is persisted as the verb of the notification
-    And a reference to SiteGuest is persisted as the actor of the notification
+    And a reference to "SiteGuest comment" is persisted as the "object" of the notification
+    And "add" is persisted as the verb of the notification
+    And a reference to "SiteGuest" is persisted as the "actor" of the notification
 
-Scenario: Trigger notification that SiteOwner deletes a post
-    Given SiteOwner deletes "My New Post"
+Scenario: Trigger notification that "SiteOwner" deletes a post
+    Given "SiteOwner" deletes the post "My New Post"
     Then a notification is created
-    And a reference to the new post is persisted as the object of the notification
-    And delete is persisted as the verb of the notification
-    And a reference to SiteOwner is persisted as the actor of the notification
+    And a reference to "My New Post" is persisted as the "object" of the notification
+    And "delete" is persisted as the verb of the notification
+    And a reference to "SiteOwner" is persisted as the "actor" of the notification
