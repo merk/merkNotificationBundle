70cdc07183ae3fc7b17800ef9526204e5c8c1cd6
IamPersistent <develop@zestic.com>
2011-08-30 11:14:29 -0700

    add generated step definitions

--- Features/Context/FeatureContext.php
+++ Features/Context/FeatureContext.php
@@ -22,16 +22,115 @@ use Behat\Gherkin\Node\PyStringNode,
  */
 class FeatureContext extends BehatContext //MinkContext if you want to test web
 {
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        $container = $this->getContainer();
-//        $container->get('some_service')->doSomethingWith($argument);
-//    }
-//
+    /**
+        * @Given /^SiteOwner creates a new post named "([^"]*)"$/
+        */
+       public function siteOwnerCreatesANewPostNamed($argument1)
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^the SiteOwner publishes "([^"]*)"$/
+        */
+       public function theSiteOwnerPublishes($argument1)
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Then /^a notification is created$/
+        */
+       public function aNotificationIsCreated()
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^a reference to the new post is persisted as the object of the notification$/
+        */
+       public function aReferenceToTheNewPostIsPersistedAsTheObjectOfTheNotification()
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^publish is persisted as the verb of the notification$/
+        */
+       public function publishIsPersistedAsTheVerbOfTheNotification()
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^a reference to SiteOwner is persisted as the actor of the notification$/
+        */
+       public function aReferenceToSiteOwnerIsPersistedAsTheActorOfTheNotification()
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^the SiteOwner updates "([^"]*)"$/
+        */
+       public function theSiteOwnerUpdates($argument1)
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^update is persisted as the verb of the notification$/
+        */
+       public function updateIsPersistedAsTheVerbOfTheNotification()
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^SiteGuest creates a comment on "([^"]*)"$/
+        */
+       public function siteGuestCreatesACommentOn($argument1)
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^a reference to the new comment is persisted as the object of the notification$/
+        */
+       public function aReferenceToTheNewCommentIsPersistedAsTheObjectOfTheNotification()
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^add is persisted as the verb of the notification$/
+        */
+       public function addIsPersistedAsTheVerbOfTheNotification()
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^a reference to SiteGuest is persisted as the actor of the notification$/
+        */
+       public function aReferenceToSiteGuestIsPersistedAsTheActorOfTheNotification()
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^SiteOwner deletes "([^"]*)"$/
+        */
+       public function siteOwnerDeletes($argument1)
+       {
+           throw new PendingException();
+       }
+
+       /**
+        * @Given /^delete is persisted as the verb of the notification$/
+        */
+       public function deleteIsPersistedAsTheVerbOfTheNotification()
+       {
+           throw new PendingException();
+       }
 }
