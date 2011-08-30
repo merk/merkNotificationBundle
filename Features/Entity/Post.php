690d7a1b1c237fc247fe8464d4d390cb1313cb0c
IamPersistent <develop@zestic.com>
2011-08-30 14:28:32 -0700

    add post class for test

---
+++ Features/Entity/Post.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace merk\NotificationBundle\Features\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="post")
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $published = false;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function publish()
+    {
+        $this->published = true;
+    }
+
+    public function isPublished()
+    {
+        return $this->published;
+    }
+}
