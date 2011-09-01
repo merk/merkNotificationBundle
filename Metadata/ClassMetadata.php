a6304e2d61db304e9b4ee5d4ada5baa4370952e5
IamPersistent <develop@zestic.com>
2011-09-01 03:06:01 -0700

    add empty ClassMetadata class

---
+++ Metadata/ClassMetadata.php
@@ -0,0 +1,12 @@
+<?php
+namespace merk\NotificationBundle\Metadata;
+
+use Metadata\ClassMetadata as BaseClassMetadata;
+
+/**
+ * @author Richard D Shank <develop@zestic.com>
+ */
+class ClassMetadata extends BaseClassMetadata
+{
+
+}
