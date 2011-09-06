300ffd0087cb370ece02958c9b2a3637b0309d01
IamPersistent <develop@zestic.com>
2011-09-06 02:21:52 -0700

    move part of NotificationSubscriber test into a Functional folder, since its scope is beyond a unit test

--- Tests/Model/NotificationSubscriberTest.php
+++ Tests/Model/NotificationSubscriberTest.php
@@ -5,12 +5,11 @@ use merk\NotificationBundle\Model\NotificationSubscriber;
 use merk\NotificationBundle\Features\Entity\Nothing;
 use merk\NotificationBundle\Features\Entity\Post;
 use merk\NotificationBundle\Features\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
  * @author Richard D Shank <develop@zestic.com>
  */
-class NotificationSubscriberTest extends WebTestCase
+class NotificationSubscriberTest extends \PHPUnit_Framework_TestCase
 {
     public function testTriggerNotification()
     {
@@ -44,33 +43,5 @@ class NotificationSubscriberTest extends WebTestCase
         $this->assertFalse($triggerNotification->invokeArgs($ns, array('not null', null)), 'null added to the trigger should have the opposite effect');
         $this->assertTrue($triggerNotification->invokeArgs($ns, array('not 3', 4)), 'null added to the trigger should have the opposite effect');
         $this->assertTrue($triggerNotification->invokeArgs($ns, array('not [a, b, c]', 'd')), 'null added to the trigger should have the opposite effect');
-    }
-
-    public function testProcess()
-    {
-        $user = new User();
-        $user->setUsername('user');
-        $post = new Post();
-        $post->setName('my post');
-        $post->publish();
-        $post->setAuthor($user);
-
-        // add listener to EventDispatch
-        $eventDispatcher = $this->getKernel()->getContainer()->get('event_dispatcher');
-
-        $ns = $this->getMockForAbstractClass('merk\NotificationBundle\Model\NotificationSubscriber', array($this->getKernel()->getContainer()));
-        $process = new \ReflectionMethod($ns,'process');
-        $process->setAccessible(true);
-        $process->invokeArgs($ns, array($post));
-
-        // use listener to test
-    }
-
-    public function getKernel(array $options = array())
-    {
-        self::$kernel = $this->createKernel($options);
-        self::$kernel->boot();
-
-        return self::$kernel;
     }
 }
